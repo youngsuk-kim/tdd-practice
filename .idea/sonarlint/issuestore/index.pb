22dd2fb8d4c27b2a13ad8a11aed3ad594fcb4dda
bread-poola <125012943+bread-poola@users.noreply.github.com>
2023-04-22 01:15:47 +0900

    test: replace

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+;
+gradlew.bat,2/a/2a45a911a8f1836b0b6c5b758962572012d8f8c3
+X
+(gradle/wrapper/gradle-wrapper.properties,f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+7
+gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void replace() {
+        String actual = "abc".replace("b", "d");
+        assertThat(actual).isEqualTo(actual);
+    }
+
+    @Test
+    void split_divide() {
+        String actual = "1,2";
+        String[] sut = actual.split(",");
+
+        assertThat(sut).contains("1", "2");
+    }
+}
