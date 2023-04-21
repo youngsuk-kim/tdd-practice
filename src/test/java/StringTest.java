3d72c806a8bc4891f733a92a909fdc56eabc6a40
bread-poola <125012943+bread-poola@users.noreply.github.com>
2023-04-22 02:08:30 +0900

    test: string test done

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -33,6 +35,16 @@ public class StringTest {
         String sut = actual.substring(1, 4);
 
         assertThat(sut).isEqualTo("1,2");
+    }
+
+    @DisplayName("스트링의 인덱스를 초과하는 로직을 작성하면 익셉션을 발생시킨다.")
+    @Test
+    void specific_char_location_throw_exception() {
+        String actual = "abc";
+
+        assertThatThrownBy(() -> {
+           actual.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
     }
 
 }
